bdc942ebd8b38f2302c81e59788045bc36ebad70
andreacappelletti97 <andreacappelletti97@gmail.com>
2020-05-25 21:51:35 +0200

    Sequences read

--- HW/host.cpp
+++ HW/host.cpp
@@ -6,14 +6,12 @@
 #include <string.h>
 #include <vector>
 #include <fstream>
+#include <string>
 
-
-#define DIM 1         //numero di coppie
-#define maxseq 3    //dimensione array seq
+#define DIM 1            //numero di coppie
+#define maxseq 3         //dimensione array seq
 #define maxstring 145390 //dimensione array string
 #define piDIM 4
-
-
 
 double run_krnl(cl::Context &context,
                 cl::CommandQueue &q,
@@ -25,285 +23,333 @@ double run_krnl(cl::Context &context,
                 std::vector<int, aligned_allocator<int>> &vPi,
                 std::vector<int, aligned_allocator<int>> &vOcc)
 {
-  cl_int err;
+    cl_int err;
 
-  // These commands will allocate memory on the FPGA. The cl::Buffer objects can
-  // be used to reference the memory locations on the device.
-  //Creating Buffers
+    // These commands will allocate memory on the FPGA. The cl::Buffer objects can
+    // be used to reference the memory locations on the device.
+    //Creating Buffers
 
-  
+    std::cout << "RUN KERNEL ENTER" << std::endl;
 
+    OCL_CHECK(err,
+              cl::Buffer buffer_stringdim(context,
 
-  std::cout << "RUN KERNEL ENTER" << std::endl;
+                                          CL_MEM_USE_HOST_PTR |
+                                              CL_MEM_READ_ONLY,
+                                          sizeof(int) * DIM,
+                                          vStringdim.data(),
+                                          &err));
+    OCL_CHECK(err,
+              cl::Buffer buffer_seqdim(context,
 
-  OCL_CHECK(err,
-            cl::Buffer buffer_stringdim(context,
+                                       CL_MEM_USE_HOST_PTR |
+                                           CL_MEM_READ_ONLY,
+                                       sizeof(int) * DIM,
+                                       vSeqdim.data(),
+                                       &err));
 
-                                        CL_MEM_USE_HOST_PTR |
-                                            CL_MEM_READ_ONLY,
-                                        sizeof(int) * DIM,
-                                        vStringdim.data(),
-                                        &err));
-  OCL_CHECK(err,
-            cl::Buffer buffer_seqdim(context,
+    OCL_CHECK(err,
+              cl::Buffer buffer_string(context,
 
-                                     CL_MEM_USE_HOST_PTR |
-                                         CL_MEM_READ_ONLY,
-                                     sizeof(int) * DIM,
-                                     vSeqdim.data(),
-                                     &err));
+                                       CL_MEM_USE_HOST_PTR |
+                                           CL_MEM_READ_ONLY,
+                                       sizeof(char) * maxstring,
+                                       vString.data(),
+                                       &err));
 
-  OCL_CHECK(err,
-            cl::Buffer buffer_string(context,
+    OCL_CHECK(err,
+              cl::Buffer buffer_seq(context,
 
-                                     CL_MEM_USE_HOST_PTR |
-                                         CL_MEM_READ_ONLY,
-                                     sizeof(char) * maxstring,
-                                     vString.data(),
-                                     &err));
+                                    CL_MEM_USE_HOST_PTR |
+                                        CL_MEM_READ_ONLY,
+                                    sizeof(char) * maxseq,
+                                    vSeq.data(),
+                                    &err));
+    OCL_CHECK(err,
+              cl::Buffer buffer_pi(context,
 
-  OCL_CHECK(err,
-            cl::Buffer buffer_seq(context,
+                                   CL_MEM_USE_HOST_PTR |
+                                       CL_MEM_READ_ONLY,
+                                   sizeof(int) * piDIM * DIM,
+                                   vPi.data(),
+                                   &err));
 
-                                  CL_MEM_USE_HOST_PTR |
-                                      CL_MEM_READ_ONLY,
-                                  sizeof(char) * maxseq,
-                                  vSeq.data(),
-                                  &err));
-  OCL_CHECK(err,
-            cl::Buffer buffer_pi(context,
+    OCL_CHECK(err,
+              cl::Buffer buffer_occ(context,
 
-                                 CL_MEM_USE_HOST_PTR |
-                                     CL_MEM_READ_ONLY,
-                                 sizeof(int) * piDIM * DIM,
-                                 vPi.data(),
-                                 &err));
+                                    CL_MEM_USE_HOST_PTR |
+                                        CL_MEM_WRITE_ONLY,
+                                    sizeof(int) * DIM,
+                                    vOcc.data(),
+                                    &err));
 
-  OCL_CHECK(err,
-            cl::Buffer buffer_occ(context,
+    //Setting the kernel Arguments
 
-                                  CL_MEM_USE_HOST_PTR |
-                                      CL_MEM_WRITE_ONLY,
-                                  sizeof(int) * DIM,
-                                  vOcc.data(),
-                                  &err));
+    OCL_CHECK(err, err = kernel.setArg(0, buffer_occ));
+    OCL_CHECK(err, err = (kernel).setArg(1, buffer_stringdim));
+    OCL_CHECK(err, err = (kernel).setArg(2, buffer_seqdim));
+    OCL_CHECK(err, err = (kernel).setArg(3, buffer_seq));
+    OCL_CHECK(err, err = (kernel).setArg(4, buffer_string));
 
-  //Setting the kernel Arguments
+    OCL_CHECK(err, err = (kernel).setArg(5, buffer_pi));
 
-  OCL_CHECK(err, err = kernel.setArg(0, buffer_occ));
-  OCL_CHECK(err, err = (kernel).setArg(1, buffer_stringdim));
-  OCL_CHECK(err, err = (kernel).setArg(2, buffer_seqdim));
-  OCL_CHECK(err, err = (kernel).setArg(3, buffer_seq));
-  OCL_CHECK(err, err = (kernel).setArg(4, buffer_string));
+    std::cout << "ARG SETUP DONE" << std::endl;
 
-  OCL_CHECK(err, err = (kernel).setArg(5, buffer_pi));
+    // Copy input data to Device Global Memory from HOST to board
+    OCL_CHECK(err,
+              err = q.enqueueMigrateMemObjects({buffer_occ, buffer_stringdim, buffer_seqdim, buffer_string, buffer_seq, buffer_pi},
+                                               0 /*0 means from host*/));
 
-  std::cout << "ARG SETUP DONE" << std::endl;
+    std::cout << "INPUT DATA COPIED" << std::endl;
 
-  // Copy input data to Device Global Memory from HOST to board
-  OCL_CHECK(err,
-            err = q.enqueueMigrateMemObjects({buffer_occ, buffer_stringdim, buffer_seqdim, buffer_string, buffer_seq, buffer_pi},
-                                             0  /*0 means from host*/));
+    std::chrono::duration<double> kernel_time(0);
 
-  std::cout << "INPUT DATA COPIED" << std::endl;
+    auto kernel_start = std::chrono::high_resolution_clock::now();
 
-  std::chrono::duration<double> kernel_time(0);
+    //Execution of the kernel KMP
+    OCL_CHECK(err, err = q.enqueueTask(kernel));
 
-  auto kernel_start = std::chrono::high_resolution_clock::now();
+    auto kernel_end = std::chrono::high_resolution_clock::now();
 
-  //Execution of the kernel KMP
-  OCL_CHECK(err, err = q.enqueueTask(kernel));
+    kernel_time = std::chrono::duration<double>(kernel_end - kernel_start);
 
-  auto kernel_end = std::chrono::high_resolution_clock::now();
+    std::cout << "KERNEL EXE COMPLETED" << std::endl;
 
-  kernel_time = std::chrono::duration<double>(kernel_end - kernel_start);
+    // Copy Result from Device Global Memory to Host Local Memory
+    OCL_CHECK(err,
+              err = q.enqueueMigrateMemObjects({buffer_occ},
+                                               CL_MIGRATE_MEM_OBJECT_HOST));
 
-  std::cout << "KERNEL EXE COMPLETED" << std::endl;
+    q.finish();
 
-  // Copy Result from Device Global Memory to Host Local Memory
-  OCL_CHECK(err,
-            err = q.enqueueMigrateMemObjects({buffer_occ},
-                                             CL_MIGRATE_MEM_OBJECT_HOST));
-
-  q.finish();
-
-  return kernel_time.count();
+    return kernel_time.count();
 }
 
+//Function to read input string and pattern
+void getPattern(std::vector<char, aligned_allocator<char>> &pattern, std::vector<int, aligned_allocator<int>> &vSeqdim)
+{
 
+    std::cout << std::endl;
+    bool firstRead = true;
+    std::string line;
+    std::string pat;
+    std::ifstream inputFile;
+    inputFile.open("pattern.txt");
+    while (getline(inputFile, line))
+    {
+        std::cout << "line " << line << std::endl;
+        //New sequence is starting
+        if (line.substr(0, 2) == "SQ" && isdigit(line[2]) && line[3] == ':')
+        {
+            //First read
+            if (firstRead)
+            {
+                std::cout << "fist read" << std::endl;
+                pat += line;
+                firstRead = false;
+            }
+            //Push the new sequence into the array
+            else
+            {
+                std::cout << "new sq" << std::endl;
+
+                for (unsigned int i = 4; i < pat.size(); i++)
+                {
+                    pattern.push_back(pat[i]);
+                }
+                std::cout << "dim update" << std::endl;
+                vSeqdim.push_back(pat.size() - 4);
+
+                pat = line;
+            }
+        }
+        else if (line.substr(0, 3) == "END")
+        {
+            for (unsigned int i = 4; i < pat.size(); i++)
+            {
+                pattern.push_back(pat[i]);
+            }
+            std::cout << "dim update" << std::endl;
+            vSeqdim.push_back(pat.size() - 4);
+            std::cout << "end" << std::endl;
+            break;
+        }
+
+        else
+        {
+            std::cout << "always pat" << std::endl;
+            pat += line;
+        }
+    }
+
+    vSeqdim.erase(vSeqdim.begin());
+
+    for (unsigned int i = 0; i < pattern.size(); i++)
+    {
+        std::cout << pattern[i];
+    }
+    std::cout << std::endl;
+
+    for (unsigned int i = 0; i < vSeqdim.size(); i++)
+    {
+        std::cout << vSeqdim[i];
+    }
+    std::cout << std::endl;
+}
 
 //Function to read input string and pattern
-void getInputString(std::vector<char, aligned_allocator<char>> &string, int type)
+void getString(std::vector<char, aligned_allocator<char>> &string)
 {
-  std::ifstream inputFile;
-  if (type == 0)
-  {
+    std::ifstream inputFile;
 
     inputFile.open("proteine.txt");
-  }
-  else
-  {
 
-    inputFile.open("pattern.txt");
-  }
+    while (!inputFile.eof())
+    {
+        char c;
+        inputFile >> c;
+        string.push_back(c);
+    }
+    string.pop_back();
+}
 
-  while (!inputFile.eof())
-  {
-    char c;
-    inputFile >> c;
-    string.push_back(c);
-  }
-  string.pop_back();
+void writeOutput(std::vector<int, aligned_allocator<int>> &occ)
+{
 }
 
 //Function to print vector content
 void printVectorContent(std::vector<char, aligned_allocator<char>> &repeat)
 {
-  for (unsigned int i = 0; i < repeat.size(); i++)
-  {
-    std::cout << i << " " << repeat[i] << std::endl;
-  }
+    for (unsigned int i = 0; i < repeat.size(); i++)
+    {
+        std::cout << i << " " << repeat[i] << std::endl;
+    }
 }
-
 
 void failure_function(std::vector<char, aligned_allocator<char>> &seq, std::vector<int, aligned_allocator<int>> &seqdim, int *pi)
 {
 
-for(unsigned int n =0; n <DIM; n++){
-
-
-  int seq_count = 0; //scorre le sequenze
-  int pi_count = 0;  //scorre pi
-
- 
-    pi[pi_count] = -1;    //first element always equal to -1
-    pi[pi_count + 1] = 0; //second element always equal to 0
-
-    for (int i = 1; i < seqdim[n]; i++)
+    for (unsigned int n = 0; n < DIM; n++)
     {
-      if (seq[seq_count + i] == seq[seq_count + pi[pi_count + i]])
-        pi[pi_count + i + 1] = pi[pi_count + i] + 1;
-      else
-      {
-        if (seq[seq_count + i] == seq[seq_count])
-          pi[pi_count + i + 1] = pi[pi_count + i];
-        else
-          pi[pi_count + i + 1] = 0;
-      }
+
+        int seq_count = 0; //scorre le sequenze
+        int pi_count = 0;  //scorre pi
+
+        pi[pi_count] = -1;    //first element always equal to -1
+        pi[pi_count + 1] = 0; //second element always equal to 0
+
+        for (int i = 1; i < seqdim[n]; i++)
+        {
+            if (seq[seq_count + i] == seq[seq_count + pi[pi_count + i]])
+                pi[pi_count + i + 1] = pi[pi_count + i] + 1;
+            else
+            {
+                if (seq[seq_count + i] == seq[seq_count])
+                    pi[pi_count + i + 1] = pi[pi_count + i];
+                else
+                    pi[pi_count + i + 1] = 0;
+            }
+        }
+
+        pi_count = pi_count + seqdim[n] + 1;
+        seq_count = seq_count + seqdim[n];
     }
-
-    pi_count = pi_count + seqdim[n] + 1;
-    seq_count = seq_count + seqdim[n];
-  }
 }
-
-
 
 int main(int argc, char **argv)
 {
-  //Define the platform = devices + context + queues
-  cl_int err;
-  cl::Context context;
-  cl::CommandQueue q;
-  cl::Kernel kernel;
-  std::string binaryFile = argv[1];
+    //Define the platform = devices + context + queues
+    cl_int err;
+    cl::Context context;
+    cl::CommandQueue q;
+    cl::Kernel kernel;
+    std::string binaryFile = argv[1];
 
-  // The get_xil_devices will return vector of Xilinx Devices
-  auto devices = xcl::get_devices("Xilinx");
+    // The get_xil_devices will return vector of Xilinx Devices
+    auto devices = xcl::get_devices("Xilinx");
 
-  // read_binary_file() command will find the OpenCL binary file created using the
-  // V++ compiler load into OpenCL Binary and return pointer to file buffer.
+    // read_binary_file() command will find the OpenCL binary file created using the
+    // V++ compiler load into OpenCL Binary and return pointer to file buffer.
 
-  auto fileBuf = xcl::read_binary_file(binaryFile);
+    auto fileBuf = xcl::read_binary_file(binaryFile);
 
-  cl::Program::Binaries bins{{fileBuf.data(), fileBuf.size()}};
-  int valid_device = 0;
-  for (unsigned int i = 0; i < devices.size(); i++)
-  {
-    auto device = devices[i];
-    // Creating Context and Command Queue for selected Device
-    OCL_CHECK(err, context = cl::Context(device, NULL, NULL, NULL, &err));
-    OCL_CHECK(err,
-              q = cl::CommandQueue(
-                  context, device, CL_QUEUE_PROFILING_ENABLE, &err));
-
-    std::cout << "Trying to program device[" << i
-              << "]: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
-    cl::Program program(context, {device}, bins, NULL, &err);
-    if (err != CL_SUCCESS)
+    cl::Program::Binaries bins{{fileBuf.data(), fileBuf.size()}};
+    int valid_device = 0;
+    for (unsigned int i = 0; i < devices.size(); i++)
     {
-      std::cout << "Failed to program device[" << i
-                << "] with xclbin file!\n";
+        auto device = devices[i];
+        // Creating Context and Command Queue for selected Device
+        OCL_CHECK(err, context = cl::Context(device, NULL, NULL, NULL, &err));
+        OCL_CHECK(err,
+                  q = cl::CommandQueue(
+                      context, device, CL_QUEUE_PROFILING_ENABLE, &err));
+
+        std::cout << "Trying to program device[" << i
+                  << "]: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
+        cl::Program program(context, {device}, bins, NULL, &err);
+        if (err != CL_SUCCESS)
+        {
+            std::cout << "Failed to program device[" << i
+                      << "] with xclbin file!\n";
+        }
+        else
+        {
+            std::cout << "Device[" << i << "]: program successful!\n";
+            OCL_CHECK(err,
+                      kernel = cl::Kernel(program, "kmp", &err));
+            valid_device++;
+            break; // we break because we found a valid device
+        }
     }
-    else
+    if (valid_device == 0)
     {
-      std::cout << "Device[" << i << "]: program successful!\n";
-      OCL_CHECK(err,
-                kernel = cl::Kernel(program, "kmp", &err));
-      valid_device++;
-      break; // we break because we found a valid device
+        std::cout << "Failed to program any device found, exit!\n";
+        exit(EXIT_FAILURE);
     }
-  }
-  if (valid_device == 0)
-  {
-    std::cout << "Failed to program any device found, exit!\n";
-    exit(EXIT_FAILURE);
-  }
 
-  std::vector<char, aligned_allocator<char>> string;
-  std::vector<char, aligned_allocator<char>> pattern;
-  std::vector<int, aligned_allocator<int>> vStringdim(DIM);
-  std::vector<int, aligned_allocator<int>> vSeqdim(DIM);
-  std::vector<int, aligned_allocator<int>> vOcc(DIM);
-  std::vector<int, aligned_allocator<int>> vPi(piDIM * DIM);
-  int pi[piDIM * DIM];
+    std::vector<char, aligned_allocator<char>> string;
+    std::vector<char, aligned_allocator<char>> pattern;
+    std::vector<int, aligned_allocator<int>> vStringdim(DIM);
+    std::vector<int, aligned_allocator<int>> vSeqdim(DIM);
+    std::vector<int, aligned_allocator<int>> vOcc(DIM);
+    std::vector<int, aligned_allocator<int>> vPi(piDIM * DIM);
+    int pi[piDIM * DIM];
 
-  //Set static dimension to try
-  vSeqdim[0] = 3;
-  vStringdim[0] = 145390;
+    //Set static dimension to try
 
+    vStringdim[0] = 145390;
 
-  //Read input string
-  getInputString(string, 0);
-  //printVectorContent(string);
-  //Read pattern to search into the string
-  getInputString(pattern, 1);
-  printVectorContent(pattern);
-//Compute failure function
-failure_function(pattern, vSeqdim, pi); 
+    //Read input string
+    getString(string);
 
-for (int i = 0; i < piDIM * DIM; i++)
-  {
-    vPi[i] = pi[i];
+    //Read pattern to search into the string
+    getPattern(pattern, vSeqdim);
 
-  }
+    //Compute failure function
+    failure_function(pattern, vSeqdim, pi);
 
-for (int i = 0; i < piDIM * DIM; i++)
-  {
-    vOcc[i] = -1;
-  }
+    for (int i = 0; i < piDIM * DIM; i++)
+    {
+        vPi[i] = pi[i];
+    }
 
+    for (int i = 0; i < piDIM * DIM; i++)
+    {
+        vOcc[i] = -1;
+    }
 
+    //Call run kernel function
+    double kernel_time_in_sec = 0;
 
+    kernel_time_in_sec = run_krnl(context,
+                                  q,
+                                  kernel,
+                                  vStringdim,
+                                  vSeqdim,
+                                  string,
+                                  pattern,
+                                  vPi,
+                                  vOcc);
 
-
-
- //Call run kernel function
-  double kernel_time_in_sec = 0;
-
-  kernel_time_in_sec = run_krnl(context,
-                                q,
-                                kernel,
-                                vStringdim,
-                                vSeqdim,
-                                string,
-                                pattern,
-                                vPi,
-                                vOcc);
-
-  std::cout << "Total time in seconds: " << kernel_time_in_sec << std::endl;
-
-
-
-
+    std::cout << "Total time in seconds: " << kernel_time_in_sec << std::endl;
 }
