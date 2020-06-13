f54abced832cf128b88f0ee0f147ffbe6c9bd9a0
andreacappelletti97 <andreacappelletti97@gmail.com>
2020-06-13 14:02:52 +0200

    Input file edit

--- RA software/RA_Functions.py
+++ RA software/RA_Functions.py
@@ -410,6 +410,7 @@ def printresult(repeats,species,tabs=""):
 def readdatafromfile(file, species):
 	if "." not in file:
 		file+=".txt"
+	file = './input' + file
 	if os.path.isfile(file):
 		for numpass in range(1,3):
 			print "Pass", numpass, "in progress..."
