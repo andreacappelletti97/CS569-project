13672bdc7320bba2873256426139eb5ced78bdb9
andreacappelletti97 <andreacappelletti97@gmail.com>
2020-06-13 11:54:47 +0200

    Print result fix

--- RA software/RA_Interface.py
+++ RA software/RA_Interface.py
@@ -156,7 +156,8 @@ def matchProtein(species,input):
 			
 			
 	print(res)	
-	print(repeatList.total_time)
+	print("reslist")
+	print(repeatList)
 	#process=multiprocessing.Process(target=popupWindow, args=("Amino Acid Match", res))
 	#process.daemon = True
 	#process.start()
