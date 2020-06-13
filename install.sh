b2fdca82751f4f5134b53974e42fa78dc5443c92
andreacappelletti97 <andreacappelletti97@gmail.com>
2020-06-13 12:28:07 +0200

    Update installation script

--- install.sh
+++ install.sh
@@ -1,19 +1,25 @@
+#!bash
+#Installation script
+
+#Install cur
 sudo apt-get install curl
 
+#Install anaconda
 cd /tmp
-
 curl –O https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
-
 bash Anaconda3-2020.02-Linux-x86_64.sh
 
+#Export conda as a global variable
 export PATH=~/anaconda3/bin:$PATH
 
+#Create conda environment with python 2.7
 conda create --name py2 python=2.7
-
+#Source necessary files
 source ~/anaconda3/etc/profile.d/conda.sh
-
+#Activate conda environment
 conda activate py2
 
+#Install required packages
 conda install numpy
 
 conda install basemap
